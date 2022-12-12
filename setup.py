59adb0c9ae95a7bb855c14e9026286545d430b3a
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2022-12-11 22:04:31 -0300

    init

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+
+setup(name='proyecto-tis',
+      version='1.0.0',
+      description='',
+      author='Juan Pablo Nahuelpán',
+      author_email='juanpablonahuelpan@gmail.com',
+      url=''
+      )
+
