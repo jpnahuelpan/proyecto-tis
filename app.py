a3402f396ddf2e02f291d1af33577484de9c7288
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2022-12-21 21:41:27 -0300

    se completó el login y registro de usuario

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: Juan Pablo Nahuelpán
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 # modulos de la aplicación
@@ -13,11 +13,11 @@ from proyecto_tis import (
 import config
 
 
-
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
+
+@app.get("/login", response_class=HTMLResponse)
 async def login(request: Request):
     stacked_email = ""
     stacked_password = ""
@@ -30,7 +30,7 @@ async def login(request: Request):
             })
 
 
-@app.post("/", response_class=RedirectResponse)
+@app.post("/login", response_class=RedirectResponse)
 async def validation(
     request: Request,
     stacked_email: str = Form(None),
@@ -39,29 +39,81 @@ async def validation(
 
     print(stacked_email, stacked_password)
     con = create_connection(config.USER_DB)
-    cursor = con.cursor()
-    q = Query(cursor)
+    q = Query(con)
     email_registered = q.email_registered(stacked_email)
     if email_registered:
-        id_pass = q.get_password_id(stacked_email)
+        id_pass = q.get_password_id_from_email(stacked_email)
         password = q.get_password(id_pass)
         if password == stacked_password:
-            print("Acceso aprovado!")
             redirect_url = request.url_for('entry')
             print(redirect_url)
-            return RedirectResponse(redirect_url)
-    # else:
-    #     return RedirectResponse("login")
+            return RedirectResponse(
+                redirect_url,
+                status_code=status.HTTP_303_SEE_OTHER,
+                )
+        else:
+            redirect_url = request.url_for('olvido')
+            return RedirectResponse(
 
-    # return templates.TemplateResponse(
-    #     "login.html",
-    #     {
-    #         "request": request,
-    #         "stacked_email": stacked_email,
-    #         "stacked_password": stacked_password,
-    #         })
+            )
+    else:
+        redirect_url = request.url_for('register')
+        return RedirectResponse(
+            redirect_url,
+            status_code=status.HTTP_303_SEE_OTHER,
+            )
 
 
-@app.post('/entry')
+@app.get('/entry')
 async def entry():
     return "Acceso aprovado!"
+
+
+# @app.post('/ovido', response_class=RedirectResponse)
+# async def olvido(request: Request):
+
+
+@app.get('/register', response_class=HTMLResponse)
+async def register_form(request: Request):
+    user_name = ""
+    a_paterno = ""
+    a_materno = ""
+    stacked_email = ""
+    stacked_password = ""
+    return templates.TemplateResponse(
+        "register.html",
+        {
+            "request": request,
+            "user_name": user_name,
+            "a_paterno": a_paterno,
+            "a_materno": a_materno,
+            "stacked_email": stacked_email,
+            "stacked_password": stacked_password,
+            })
+
+
+@app.post("/register", response_class=RedirectResponse)
+async def register(
+    request: Request,
+    user_name: str = Form(),
+    a_paterno: str = Form(),
+    a_materno: str = Form(),
+    stacked_email: str = Form(),
+    stacked_password: str = Form(),
+        ):
+    user_data = {
+        "name": user_name,
+        "a_paterno": a_paterno,
+        "a_materno": a_materno,
+        "email": stacked_email,
+        "password": stacked_password,
+    }
+    print(user_data)
+    con = create_connection(config.USER_DB)
+    q = Query(con)
+    q.insert_user(user_data)
+    redirect_url = request.url_for('login')
+    return RedirectResponse(
+        redirect_url,
+        status_code=status.HTTP_303_SEE_OTHER,
+        )
--- proyecto_tis/query.py
+++ proyecto_tis/query.py
@@ -2,32 +2,78 @@
 # las consultas necesarias para el sistema.
 
 class Query():
-    def __init__(self, cursor):
-        self._cursor = cursor
+    def __init__(self, con):
+        self._con = con
+        self._cursor = con.cursor()
 
     def email_registered(self, email):
         query = f"""
-            select eMail from email where eMail='{email}';
+            select eMail from email where eMail={email!r};
         """
-        response = self._cursor.execute(query)
-        response = response.fetchall()[0][0]
-        if response:
+        try:
+            response = self._cursor.execute(query)
+            response = response.fetchall()[0][0]
             return True
-        else:
+        except IndexError:
             return False
 
-    def get_password_id(self, email):
+    def get_email_id(self, email):
         query = f"""
-            select eMailId from email where eMail='{email}';
+            select eMailId from email where eMail={email!r}
+        """
+        response = self._cursor.execute(query)
+        response = response.fetchall()
+        return response
+
+    def get_password_id(self, password):
+        query = f"""
+            select passId from password where password={password!r}
         """
         response = self._cursor.execute(query)
         response = response.fetchall()[0][0]
+        return response
+
+    def get_password_id_from_email(self, email):
+        query = f"""
+            select passId from email where eMail={email!r}
+        """
+        response = self._cursor.execute(query)
+        response = response.fetchone()[0]
         return response
 
     def get_password(self, pass_id):
         query = f"""
-            select password from password where passId={pass_id};
+            select password from password where passId={pass_id}
         """
         response = self._cursor.execute(query)
         response = response.fetchall()[0][0]
         return response
+
+    def insert_user(self, user_data):
+        password = f"""
+            insert into password (password) values(
+                {user_data["password"]!r}
+            )
+        """
+        self._cursor.execute(password)
+        self._con.commit()
+        pass_id = self.get_password_id(user_data["password"])
+        email = f"""
+            insert into email (email, passId) values (
+                {user_data["email"]!r},
+                {pass_id}
+            )
+        """
+        self._cursor.execute(email)
+        self._con.commit()
+        email_id = self.get_email_id(user_data["email"])
+        user = f"""
+            insert into user (nombre, apellidoP, apellidoM, eMailId) values(
+                {user_data["name"]!r},
+                {user_data["a_paterno"]!r},
+                {user_data["a_materno"]!r},
+                {email_id}
+            )
+        """
+        self._cursor.execute(user)
+        self._con.commit()
